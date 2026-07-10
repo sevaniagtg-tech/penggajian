e72c34e27ffc64c807639c67fea18845821d5fbb
sevania <sevania@.com>
2026-07-10 17:27:22 +0700

    menyimpan

---
+++ dashboard.js
@@ -0,0 +1,17 @@
+function bukaKaryawan() {
+    window.location.href = "karyawan.html";
+}
+
+function bukaPenggajian() {
+    window.location.href = "penggajian.html";
+}
+
+function bukaLaporan() {
+    window.location.href = "laporan.html";
+}
+
+function logout() {
+    if (confirm("Yakin ingin logout?")) {
+        window.location.href = "login.html";
+    }
+}
